33351406cfa4826b75719477b9f53fa312d76c26
RomanovM <maxim.r@nopcommerce.com>
2021-12-07 16:54:40 +0200

    Test crlf

--- src/Libraries/Nop.Core/BaseEntity.cs
+++ src/Libraries/Nop.Core/BaseEntity.cs
@@ -1,10 +1,11 @@
 ﻿namespace Nop.Core
 {
     /// <summary>
-    /// Represents the base class for entities
+    /// Represents the base class for entities 
     /// </summary>
     public abstract partial class BaseEntity
     {
+        //test commit
         /// <summary>
         /// Gets or sets the entity identifier
         /// </summary>
